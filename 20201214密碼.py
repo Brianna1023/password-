39f48a89fdbfb13cde1e444f51d53e3437f5fbda
Brianna Syu <chelle_2198@yahoo.com.tw>
2020-12-14 12:17:43 +0800

    final version

--- 20201214密碼.py
+++ 20201214密碼.py
@@ -1,12 +1,15 @@
 password = 'brianna1023'
 i = 3
-while True:
+while i > 0:
 	psd = input('請問帳號密碼:')
 	if psd == password:
 		print('登入成功!!')
 		break
 	else:
 		i = i - 1
-		print('密碼錯誤還有', i, '次機會')
-		if i == 0:
-			break
+		print('密碼錯誤')
+		if i > 0:
+			print('還有', i, '次機會')
+		else:
+			print('沒機會嘗試了!要鎖帳號了!')
+		
